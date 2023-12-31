812bb177e73e41f4c1122251817608e14ef01f78
Shubham-355 <smsuthar2520@gmail.com>
2023-12-31 22:16:26 +0530

    hello

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+function blockIndianUsers() {
+    console.log("indian fn called")
+    fetch('https://api.country.is/')
+        .then(response => response.json())
+        .then(data => {
+            console.log("this is data", data);
+            const userCountry = data.country;
+            console.log("this is ", userCountry);
+            // Check if the user is from India (country code 'IN')
+            if (userCountry === 'IN') {
+                const intro = document.getElementsByClassName("intro")[0];
+                console.log(intro);
+                while (intro.firstChild) {
+                    intro.removeChild(intro.firstChild);
+                }
+                // Show the custom alert box
+                document.getElementById('overlay').style.display = 'block';
+                document.getElementById('customAlert').style.display = 'block';
+            }
+        })
+        .catch(error => console.error('Error fetching IP information:', error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    blockIndianUsers();
+});
+
+
 function showMaterial(materialClass) {
     const materialContainers = document.querySelectorAll('.material-container');
     materialContainers.forEach(container => {
